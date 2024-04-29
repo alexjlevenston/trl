961cdf6ef89bd4dac62e6ab4a60f438e54618509
Alexander Levenston <alexanderlevenston@Alexanders-MacBook-Air.local>
2024-04-29 14:39:00 -0400

    implement concatenated_inputs method for rebel trainer, and add attention_mask to build_tokenized_answers

--- trl/trainer/rebel_trainer.py
+++ trl/trainer/rebel_trainer.py
@@ -444,9 +444,11 @@ class REBELTrainer(Trainer):
         player_id_to_loss_code = lambda player_id: LossType.XENTROPY if player_id == -1 else LossType.REBEL
         
         full_input_ids = prompt_tokens["input_ids"] + answer_tokens["input_ids"]
+        full_attention_mask = prompt_tokens["attention_mask"] + answer_tokens["attention_mask"]
         full_player_ids = [-1 for i in range(len(prompt_tokens["input_ids"]))] + player_ids
         full_loss_codes = prompt_loss_codes + [player_id_to_loss_code(player_id) for player_id in player_ids]
         input_ids = full_input_ids[:-1]
+        attention_mask = full_attention_mask[:-1]
         target_ids = full_input_ids[1:]
         player_ids = full_player_ids[1:]
         loss_codes = full_loss_codes[1:]
@@ -459,6 +461,7 @@ class REBELTrainer(Trainer):
             player_ids=np.array(player_ids),
             loss_codes=np.array(loss_codes),
             rewards=np.array(rewards),
+            attention_mask=np.array(attention_mask),
         )
 
     def tokenize_row(self, feature, model: Optional[Union[PreTrainedModel, nn.Module]] = None) -> Dict:
@@ -526,63 +529,46 @@ class REBELTrainer(Trainer):
 
     @staticmethod
     def concatenated_inputs(
-        batch: Dict[str, Union[List, torch.LongTensor]],
-        is_encoder_decoder: bool = False,
-        label_pad_token_id: int = -100,
-        padding_value: int = 0,
+        batch: Dict[str, Union[torch.LongTensor]],
         device: Optional[torch.device] = None,
     ) -> Dict[str, torch.LongTensor]:
         """Concatenate the chosen and rejected inputs into a single tensor.
 
         Args:
-            batch: A batch of data. Must contain the keys 'chosen_input_ids' and 'rejected_input_ids', which are tensors of shape (batch_size, sequence_length).
-            is_encoder_decoder: Whether the model is an encoder-decoder model.
-            label_pad_token_id: The label pad token id.
-            padding_value: The padding value to use for the concatenated inputs_ids.
+            batch: A batch of data. Values are tensors of shape (batch_size, ?).
             device: The device for the concatenated inputs.
 
         Returns:
-            A dictionary containing the concatenated inputs under the key 'concatenated_input_ids'.
+            A dictionary containing the concatenated batch tensors.
         """
         concatenated_batch = {}
 
-        if is_encoder_decoder:
-            max_length = max(batch["chosen_labels"].shape[1], batch["rejected_labels"].shape[1])
-        else:
-            max_length = max(batch["chosen_input_ids"].shape[1], batch["rejected_input_ids"].shape[1])
-
-        for k in batch:
-            if k.startswith("chosen") and isinstance(batch[k], torch.Tensor):
-                if "labels" in k or is_encoder_decoder:
-                    pad_value = label_pad_token_id
-                elif k.endswith("_input_ids"):
-                    pad_value = padding_value
-                elif k.endswith("_attention_mask"):
-                    pad_value = 0
-                concatenated_key = k.replace("chosen", "concatenated")
-                concatenated_batch[concatenated_key] = pad_to_length(batch[k], max_length, pad_value=pad_value)
-        for k in batch:
-            if k.startswith("rejected") and isinstance(batch[k], torch.Tensor):
-                if "labels" in k or is_encoder_decoder:
-                    pad_value = label_pad_token_id
-                elif k.endswith("_input_ids"):
-                    pad_value = padding_value
-                elif k.endswith("_attention_mask"):
-                    pad_value = 0
-                concatenated_key = k.replace("rejected", "concatenated")
+        for k in sorted(batch): # so chosen_ keys always come first
+            max_length = max(batch[k.replace("rejected", "chosen")].shape[1], batch[k.replace("chosen", "rejected")].shape[1])
+            if k.endswith("player_ids"):
+                pad_value = -1
+            elif k.endswith("ids"):
+                pad_value = self.pad_token_id
+            elif k.endswith("mask"):
+                pad_value = 0
+            elif k.endswith("rewards"):
+                pad_value = 0
+            elif k.endswith("loss_codes"):
+                pad_value = LossType.NONE
+            else:
+                raise ValueError(f"Unexpected key encountered in data collator: {k}")
+            concatenated_key = k.replace("chosen", "concatenated").replace("rejected", "concatenated")
+            padded = pad_to_length(batch[k], max_length, pad_value=pad_value)
+            if concatenated_key not in concatenated_batch:
+                concatenated_batch[concatenated_key] = padded
+            else:
                 concatenated_batch[concatenated_key] = torch.cat(
                     (
                         concatenated_batch[concatenated_key],
-                        pad_to_length(batch[k], max_length, pad_value=pad_value),
+                        padded,
                     ),
                     dim=0,
-                ).to(device=device)
-
-        if is_encoder_decoder:
-            concatenated_batch["concatenated_input_ids"] = batch["prompt_input_ids"].repeat(2, 1).to(device=device)
-            concatenated_batch["concatenated_attention_mask"] = (
-                batch["prompt_attention_mask"].repeat(2, 1).to(device=device)
-            )
+                )
 
         return concatenated_batch
 
